7334fbd5f084e5b7da1b26a069964193c16f7191
en-arnob <en.arnob@gmail.com>
2022-03-27 15:33:54 +0600

    git commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = require("./routes");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Softcent!");
+});
+app.use(router);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server started successfully on Port: ${PORT}`);
+});
---
+++ controller.js
@@ -0,0 +1,48 @@
+const request = require("request");
+
+exports.apiGetController = (req, res, next) => {
+  let currency = req.query.currency.toLowerCase();
+  //   console.log(currency);
+
+  let today = new Date();
+  let priorDate = new Date(new Date().setDate(today.getDate() - 30));
+  let todayFormatted = new Date().toISOString().slice(0, 10);
+  let priorDateFormatted = priorDate.toISOString().slice(0, 10);
+  //   console.log(priorDateFormatted);
+  //   console.log(todayFormatted);
+
+  if (currency === "usd" || currency === "eur" || currency === "gbp") {
+    request.get(
+      {
+        url: `https://api.coindesk.com/v1/bpi/historical/close.json?start=${priorDateFormatted}&end=${todayFormatted}&currency=${currency}`,
+        json: true,
+      },
+      (error, response) => {
+        if (error) {
+          return res.send({
+            msg: "Error occured fetching data from coindesk api",
+          });
+        }
+        let obj = response.body.bpi;
+        let rates = Object.values(obj);
+        // console.log(rates);
+        let maxRate = Math.max(...rates);
+        let minRate = Math.min(...rates);
+        newObj = {
+          requestedCurrency: currency.toUpperCase(),
+          currentBitCoinRate: obj[Object.keys(obj)[29]],
+          lowestBitCoinRate: minRate,
+          highestBitCoinRate: maxRate,
+          interval: "30 Days",
+        };
+
+        // console.log(newObj);
+        return res.json(newObj);
+      }
+    );
+  } else {
+    res.json({
+      Message: "Provide a valid currency: USD/EUR/GBP",
+    });
+  }
+};
